3e444bd9f0c7be90f792dbaf839c9df0bcd28662
Izzettin Karasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-08-07 08:42:00 +0300

    Initial commit

---
+++ src/main/java/com/cmddotenter/StudentClubManagementSystem/StudentClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cmddotenter.StudentClubManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentClubManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentClubManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmddotenter/StudentClubManagementSystem/StudentClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmddotenter.StudentClubManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentClubManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
