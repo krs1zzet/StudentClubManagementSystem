23ef5a893b590bca5a27469f48399ee11e12e43a
Izzettin Karasayar <izzettin.karasayar@std.yildiz.edu.tr>
2024-10-01 23:07:09 +0300

    updated

--- src/main/java/com/cmddotenter/StudentClubManagementSystem/dto/Converter/EventDtoConverter.java
+++ src/main/java/com/cmddotenter/StudentClubManagementSystem/dto/Converter/EventDtoConverter.java
@@ -1,18 +1,15 @@
 package com.cmddotenter.StudentClubManagementSystem.dto.Converter;
 
 import com.cmddotenter.StudentClubManagementSystem.dto.EventDTO;
-import com.cmddotenter.StudentClubManagementSystem.dto.UserDTO;
 import com.cmddotenter.StudentClubManagementSystem.entity.Event;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
-public class EventDtoConverter {
+public class EventDTOConverter {
 
-
-    public EventDTO convert(Event from) {
+    public EventDTO convert(Event from){
         return new EventDTO(
                 from.getId(),
                 from.getName(),
@@ -21,12 +18,7 @@ public class EventDtoConverter {
                 from.getClub().getId()
         );
     }
-
-    public List<EventDTO> convert(List<Event> from) {
-        return from.stream().map(this::convert).collect(Collectors.toList());
+    public List<EventDTO> convert(List<Event> from){
+        return from.stream().map(this::convert).collect(java.util.stream.Collectors.toList());
     }
 }
-
-
-
-
